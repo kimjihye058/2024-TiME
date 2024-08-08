7ba07ae5f1c4a7246baa2e153132c05eae07240d
lseongmi <w2307@e-mirim.hs.kr>
2024-08-09 01:50:29 +0900

    Feat : nav 반응형 완전한 구현

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navToggle = document.getElementById('nav-toggle');
     const info = document.getElementById('info');
+    const toggleX = document.getElementById('toggle-x');
 
+    // 햄버거 아이콘 클릭 시 메뉴 열기/닫기
     navToggle.addEventListener('click', function() {
         info.classList.toggle('show-menu');
     });
 
-    
+    // X 아이콘 클릭 시 메뉴 닫기
+    toggleX.addEventListener('click', function() {
+        info.classList.remove('show-menu');
+    });
 });
